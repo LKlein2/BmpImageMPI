d2ba2efb95195c3c6d107bae3062f02f30b7a5f0
Lucas Klein - Promob <lucas.klein@promob.com>
2021-06-21 22:54:17 -0300

    Passado para vetor e finalizado

--- mpi/main.c
+++ mpi/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 #pragma pack(1)
 
 typedef struct cabecalho {
@@ -27,45 +28,45 @@ typedef struct rgb{
 	unsigned char red;
 } RGB;
 
-void lerArquivo(RGB** img, CABECALHO header, FILE *file)
+void lerArquivo(RGB* img, CABECALHO header, FILE *file)
 {
     int alinhamento;
     unsigned char byte;
 
-	for(int i = 0 ; i < header.altura ; i++) {
+	for(int i = 0; i < header.altura; i++){
 		alinhamento = (header.largura * 3) % 4;
 
 		if (alinhamento != 0){
 			alinhamento = 4 - alinhamento;
 		}
 
-		for(int j = 0 ; j < header.largura ; j++){
-			fread(&img[i][j], sizeof(RGB), 1, file);
+		for(int j = 0; j < header.largura; j++){
+			fread(&img[i * header.largura + j], sizeof(RGB), 1, file);
 		}
 
-		for(int j = 0; j<alinhamento; j++){
+		for(int j = 0; j < alinhamento; j++){
 			fread(&byte, sizeof(unsigned char), 1, file);
 		}
 	}
 }
 
-void escreverArquivo(RGB** img, CABECALHO header, FILE *file)
+void escreverArquivo(RGB* img, CABECALHO header, FILE *file)
 {
     int alinhamento;
     unsigned char byte;
 
-	for(int i = 0 ; i < header.altura ; i++) {
+	for(int i = 0; i < header.altura; i++){
 		alinhamento = (header.largura * 3) % 4;
 
 		if (alinhamento != 0){
 			alinhamento = 4 - alinhamento;
 		}
 
-		for(int j = 0 ; j < header.largura ; j++){
-			fwrite(&img[i][j], sizeof(RGB), 1, file);
+		for(int j = 0; j < header.largura; j++){
+			fwrite(&img[i * header.largura + j], sizeof(RGB), 1, file);
 		}
 
-		for(int j = 0; j<alinhamento; j++){
+		for(int j = 0; j < alinhamento; j++){
 			fwrite(&byte, sizeof(unsigned char), 1, file);
 		}
 	}
@@ -91,82 +92,75 @@ MAIN ---------------------------------------------------------------------
 int main(int argc, char **argv ){
     FILE *fileIn, *fileOut;
     CABECALHO header;
-    RGB **imgIn, **imgOut;
+    RGB *imgIn, *imgOut, *imgInAux, *imgOutAux;
     int mascara;
 
-    printf("INICIANDO PROGRAMA...\n");
+	int numProcessos, idProcesso;
+	double tempoInicial, tempoFinal;
 
+	/*
     argc = 4;
     argv[1] = "c:\\temp\\borboleta.bmp";
     argv[2] = "c:\\temp\\lixo.bmp";
     argv[3] = "7";
+	*/
 
-    //Testa o numero de argumentos
+    MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &idProcesso);
+	MPI_Comm_size(MPI_COMM_WORLD, &numProcessos);
+
     if (argc != 4) {
 		printf("%s <img_entrada> <img_saida> <mascara> \n", argv[0]);
 		exit(0);
 	}
 
-    //Abre o arquivo de input
-	printf("\nABRINDO ARQUIVO DE ENTRADA...\n");
 	fileIn = fopen(argv[1], "rb");
 	if (fileIn == NULL) {
         printf("Erro ao abrir o arquivo %s\n", argv[1]);
 		exit(0);
 	}
-    printf("ABRIU ARQUIVO DE ENTRADA\n");
 
-	//Abre o arquivo de output
-    printf("\nABRINDO ARQUIVO DE SAIDA...\n");
 	fileOut = fopen(argv[2], "wb");
 	if (fileOut == NULL){
 		printf("Erro ao abrir o arquivo %s\n", argv[2]);
 		exit(0);
 	}
-	printf("ABRIU ARQUIVO DE SAIDA\n");
 
-    //Verifica tamanho da mascara
 	mascara = atoi(argv[3]);
     if (mascara != 3 && mascara != 5 && mascara != 7) {
         printf("Mascara de %d não suportada!\n", mascara);
 		exit(0);
     }
 
-    //L� o cabe�alho do arquivo de entrada
-    printf("\nLENDO ARQUIVO DE ENTRADA...\n");
 	fread(&header, sizeof(CABECALHO), 1, fileIn);
-	printf("LEU ARQUIVO DE ENTRADA\n");
-
-	//escreve o cabe�alho do arquivo de sa�da
-	printf("\nESCREVENDO ARQUIVO DE SAIDA...\n");
 	fwrite(&header, sizeof(CABECALHO), 1, fileOut);
-    printf("ESCREVEU ARQUIVO DE SAIDA\n");
 
-    printf("\n");
-    printf("Tamanho do arquivo: %u\n", header.tamanho_arquivo);
-    printf("Offset: %d\n", header.offset);
-    printf("Largura: %d\n", header.largura);
-    printf("Altura: %d\n", header.altura);
-    printf("Bits: %d\n", header.bits);
-    printf("Tamanho da imagem: %u\n", header.tamanho_imagem);
-    printf("\n");
+    imgIn  	  = (RGB *)malloc(header.altura * header.largura * sizeof(RGB));
+	imgInAux  = (RGB *)malloc((header.altura / numProcessos) * header.largura * sizeof(RGB));
+    imgOut 	  = (RGB *)malloc(header.altura * header.largura * sizeof(RGB));
+	imgOutAux = (RGB *)malloc(header.altura * header.largura * sizeof(RGB));
 
-    //Aloca o espaço para os pixels da imagem
-    printf("\nALOCANDO MEMORIA PARA ESTRUTURAS...\n");
-    imgIn  = (RGB **)malloc(header.altura * sizeof(RGB *));
-    imgOut = (RGB **)malloc(header.altura * sizeof(RGB *));
-    for(int i = 0 ; i < header.altura ; i++) {
-		imgIn[i]  = (RGB *)malloc(header.largura * sizeof(RGB));
-		imgOut[i] = (RGB *)malloc(header.largura * sizeof(RGB));
+	if (idProcesso == 0) {
+		lerArquivo(imgIn, header, fileIn);
 	}
-	printf("ALOCOU MEMORIA PARA ESTRUTURAS\n");
 
-	//Lê a imagem original
-    printf("\nLENDO O ARQUIVO...\n");
-    lerArquivo(imgIn, header, fileIn);
-    printf("LEU O ARQUIVO\n");
+	tempoInicial = MPI_Wtime();
 
-	printf("\nPROCESSANDO A NOVA IMAGEM...\n");
+	MPI_Bcast(	imgOut, 
+			  	(header.altura * header.largura)*sizeof(RGB),
+			  	MPI_BYTE, 
+			  	0, 
+			  	MPI_COMM_WORLD);
+
+	MPI_Scatter(imgIn,
+				(header.altura / numProcessos) * header.largura * sizeof(RGB), 
+				MPI_BYTE,
+				imgInAux, 
+				(header.altura / numProcessos) * header.largura * sizeof(RGB), 
+				MPI_BYTE, 
+				0, 
+				MPI_COMM_WORLD);
+	
 	int sizeOfArray = (mascara * mascara);
 	int* arrayR = (int*)malloc(sizeOfArray * sizeof(int));
 	int* arrayG = (int*)malloc(sizeOfArray * sizeof(int));
@@ -179,10 +173,7 @@ int main(int argc, char **argv ){
 	int range = (mascara - 1) / 2;
 	int mediana = sizeOfArray / 2;	
 
-	printf("Altura: %d\n", header.altura);
-	printf("Largura: %d\n", header.largura);
-
-	for (int i = 0; i < header.altura; i++)
+	for (int i = 0; i < header.altura / numProcessos; i++)
 	{
 		for (int j = 0; j < header.largura; j++)
 		{
@@ -204,37 +195,43 @@ int main(int argc, char **argv ){
 						arrayB[posicaoArray] = 0;
 						posicaoArray++;
 					} else {
-						arrayR[posicaoArray] = imgIn[posAltura][posLargura].red;
-						arrayG[posicaoArray] = imgIn[posAltura][posLargura].green;
-						arrayB[posicaoArray] = imgIn[posAltura][posLargura].blue;
+						arrayR[posicaoArray] = imgInAux[posAltura * header.largura + posLargura].red;
+						arrayG[posicaoArray] = imgInAux[posAltura * header.largura + posLargura].green;
+						arrayB[posicaoArray] = imgInAux[posAltura * header.largura + posLargura].blue;
 						posicaoArray++;
 					}
 				}				
 			}		
 
-			imgOut[i][j].red   = ordenarVetor(arrayR, sizeOfArray)[mediana];
-			imgOut[i][j].green = ordenarVetor(arrayG, sizeOfArray)[mediana];
-			imgOut[i][j].blue  = ordenarVetor(arrayB, sizeOfArray)[mediana];
+			imgOut[i * header.largura + j].red   = ordenarVetor(arrayR, sizeOfArray)[mediana];
+			imgOut[i * header.largura + j].green = ordenarVetor(arrayG, sizeOfArray)[mediana];
+			imgOut[i * header.largura + j].blue  = ordenarVetor(arrayB, sizeOfArray)[mediana];
 		}
 	}
 
 	free(arrayR);
 	free(arrayG);
 	free(arrayB);
-	printf("PROCESSOU A NOVA IMAGEM...\n");
 
-	//Escreve a nova imagem
-	printf("\nESCREVENDO O NOVO ARQUIVO...\n");
-    escreverArquivo(imgOut, header, fileOut);
-    printf("ESCREVEU O NOVO ARQUIVO\n");
+	MPI_Gather( imgOut,
+				header.altura / numProcessos * header.largura * sizeof(RGB), 
+				MPI_BYTE,
+				imgOutAux, 
+				header.altura / numProcessos * header.largura * sizeof(RGB), 
+				MPI_BYTE, 
+				0, 
+				MPI_COMM_WORLD);
 
-    //Fecha o arquivo de input
-	printf("\nFECHANDO ARQUIVO DE ENTRADA...\n");
+	if (idProcesso == 0) {
+		escreverArquivo(imgOutAux, header, fileOut);
+	}
+
 	fclose(fileIn);
-    printf("FECHOU ARQUIVO DE ENTRADA\n");
-
-    //Fecha o arquivo de output
-    printf("\nFECHANDO ARQUIVO DE SAIDA...\n");
 	fclose(fileOut);
-	printf("FECHOU ARQUIVO DE SAIDA\n");
+
+	tempoFinal = MPI_Wtime();
+	if (idProcesso == 0) {
+		printf("\nTEMPO DE EXECUÇÃO: %f\n", tempoFinal - tempoInicial);
+	}
+	MPI_Finalize();
 }
